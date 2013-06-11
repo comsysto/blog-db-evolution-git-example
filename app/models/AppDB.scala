f33d9fea745eabe18162a0979e9ad9bc92bc5b2d
Bernd Zuther <bernd.zuther@comsysto.com>
2013-06-11 20:39:13 +0200

    add solution for the development branch

--- app/models/AppDB.scala
+++ app/models/AppDB.scala
@@ -10,10 +10,8 @@ case class BlogEntry(val title: String, val content: String) extends KeyedEntity
   val id: Long = 0
 
   //TODO-NEXT-RELEASE: remove createdAt
-  val createdAt: Date = new Date(System.currentTimeMillis)
-
   //TODO-NEXT-RELEASE: remove createdAt here too
-  override def toString = "{id : ${id}, title : ${title}, content : ${content}, createdAt : ${createdAt}}"
+  override def toString = "{id : ${id}, title : ${title}, content : ${content}}"
 }
 
 object AppDB extends Schema  {
