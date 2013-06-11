bfd515baed14eb77dd60447916bbfff10347a5cc
Bernd Zuther <bernd.zuther@comsysto.com>
2013-06-11 19:35:32 +0200

    gui looks nice new & add todos for demo

--- app/models/AppDB.scala
+++ app/models/AppDB.scala
@@ -8,8 +8,11 @@ import java.sql.{Date, Timestamp}
  */
 case class BlogEntry(val title: String, val content: String) extends KeyedEntity[Long] with java.io.Serializable {
   val id: Long = 0
+
+  //TODO-NEXT-RELEASE: remove createdAt
   val createdAt: Date = new Date(System.currentTimeMillis)
 
+  //TODO-NEXT-RELEASE: remove createdAt here too
   override def toString = "{id : ${id}, title : ${title}, content : ${content}, createdAt : ${createdAt}}"
 }
 
